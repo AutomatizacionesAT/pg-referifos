68e60b45218c63ef5e0c5431a6ca44457dfa20d0
nedagosa-cod <nestorcode4004@gmail.com>
2022-11-16 15:59:36 -0500

    creacion de estados

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => res.render("index", {title: 'Referidos Atento'}));
-router.get("/login", (req, res) => res.render("login"));
-router.get("/referir ", (req, res) => res.render("referir"));
+router.get("/referir", (req, res) => res.render("referir", {title: 'Referidos Atento'}));
+router.get("/estado", (req, res) => res.render("estado", {title: 'Referidos Atento'}));
+router.get("/login", (req, res) => res.render("login", {title: 'Referidos Atento'}));
 
 export default router;
